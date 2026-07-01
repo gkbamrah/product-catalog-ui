fcae6c4cf513d0e1abc0f13a96f4f59f512a8a67
Guramrit Bamrah <guramritb@gmail.com>
2026-07-01 17:45:58 -0400

    added form componenets

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -34,11 +34,13 @@ export const ProductList: React.FC = () => {
     }
 
     return (
-        <div className="product-list grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {products.map((product) => (
-                <ProductCard key={product.id} product={product} />
-            ))}
-        </div>
+        <>  
+            <div className="product-list grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {products.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
+        </>
     )
 
 }
